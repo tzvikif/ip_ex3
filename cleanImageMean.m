8217e229bfde7647078ef570bc1ac56d8b5c061f
tzvikif <tzvikif@gmail.com>
2019-12-31 23:57:50 +0200

    fixed bug on cleanImageMean

--- cleanImageMean.m
+++ cleanImageMean.m
@@ -1,12 +1,13 @@
 function cleanIm = cleanImageMean (im, radius, maskSTD)
     %cleanIm = zeros(imColSize,imRowSize);
     mask = zeros(radius*2+1,radius*2+1);
-    for x=1:2*radius + 1
-        for y=1:2*radius + 1
-            mask(y,x) = exp(-x.^2/(2*maskSTD^2)-y.^2/(2*maskSTD^2));
+    xcenter = 1+radius;
+    ycenter = 1+radius;
+    for x=1:2*radius+1
+        for y=1:2*radius+1
+            mask(y,x) = exp(-(xcenter-x).^2/(2*maskSTD^2)-(ycenter-y).^2/(2*maskSTD^2));
         end
     end
     mask = mask/sum(mask,'all');    % preserve mean
-    mask = circshift(mask,[2,2]);
     cleanIm = conv2(im,mask);
 end
